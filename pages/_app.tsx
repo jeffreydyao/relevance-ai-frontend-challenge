9bb12ed80aed27d0a3b8c0508ea5cc57a02efa69
Jeffrey Yao <jeffrey.yao@dijgtal.com>
2022-10-14 02:03:01 +1100

    fix: import Noto Sans individual font weights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
 import "../styles/globals.css";
 import "@fontsource/noto-sans";
+// Individual font weights
+import "@fontsource/noto-sans/400.css";
+import "@fontsource/noto-sans/500.css";
+import "@fontsource/noto-sans/600.css";
+import "@fontsource/noto-sans/700.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 
