37310c761823b9a94cd351ec217ce2b346ad697a
Jeffrey Yao <jeffrey.yao@dijgtal.com>
2022-10-14 00:48:35 +1100

    feat: Workflows history header

---
+++ mockData.ts
@@ -0,0 +1,40 @@
+export const data = {
+  results: [
+    {
+      id: "ODYyNTU1MGM0NGM5LTRlZjctYjE1Zi1jNGQyYjk3OTBhMWQ-205ec28e-1",
+      params: {
+        workflow_name: "Vectorize",
+        dataset_id: "sample-dataset",
+      },
+      status: "completed",
+      creation_time: "2022-09-08T02:32:43.591Z",
+    },
+    {
+      id: "ODYyNTU1MGM0NGM5LTRlZjctYjE1Zi1jNGQyYjk3OTBhMWQ-7696d154-6",
+      params: {
+        workflow_name: "Cluster",
+        dataset_id: "customer-survey-dataset",
+      },
+      status: "running",
+      creation_time: "2022-09-08T02:32:43.591Z",
+    },
+    {
+      id: "ODYyNTU1MGM0NGM5LTRlZjctYjE1Zi1jNGQyYjk3OTBhMWQ-cc760afc-3",
+      params: {
+        workflow_name: "AI tagging",
+        dataset_id: "new-dataset",
+      },
+      status: "completed",
+      creation_time: "2022-09-08T02:32:43.591Z",
+    },
+    {
+      id: "ODYyNTU1MGM0NGM5LTRlZjctYjE1Zi1jNGQyYjk3OTBhMWQ-e3b37cd4-5",
+      params: {
+        workflow_name: "Vectorize",
+        dataset_id: "sample-dataset",
+      },
+      status: "completed",
+      creation_time: "2022-09-08T02:32:43.591Z",
+    },
+  ],
+};
