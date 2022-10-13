f650d749392337d78b16552c6a93e1c0f78ebdab
Jeffrey Yao <jeffrey.yao@dijgtal.com>
2022-10-13 23:42:19 +1100

    feat: add declaration file for icons

---
+++ components/icons.d.ts
@@ -0,0 +1,4 @@
+declare module "@iconscout/react-unicons/icons/uil-angle-down";
+declare module "@iconscout/react-unicons/icons/uil-link";
+declare module "@iconscout/react-unicons/icons/uil-question-circle";
+declare module "@iconscout/react-unicons/icons/uil-database";
